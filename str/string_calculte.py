a17c17a4ff674948edadb52d567ab9391cdadab6
Yacine Ghenabzia <yacineghenabzia@gmail.com>
2025-10-29 18:26:09 +0000

    add test and code  number 2

--- str/string_calculte.py
+++ str/string_calculte.py
@@ -1,4 +1,6 @@
 def stringcalcult(nubmer:str)->int:
     if nubmer=="1":
         return 1
+    if nubmer=="2":
+        return 2
     return  0
--- tests/test_string_calculte.py
+++ tests/test_string_calculte.py
@@ -1,7 +1,7 @@
+import pytest
+
 from str.string_calculte import stringcalcult
 
-
-def test_calculte_string_0_return_0():
-    assert stringcalcult("0")==0
-def test_calculte_string_1_return_1():
-    assert stringcalcult("1")==1
+@pytest.mark.parametrize("number,expected",[("0",0),("1",1),("2",2)])
+def test_calculte_string_0_return_0(number,expected):
+    assert stringcalcult(number) == expected
