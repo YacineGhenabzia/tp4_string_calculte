a7737b7c931bbfae94ec50c28f40b523491441cb
Yacine Ghenabzia <yacineghenabzia@gmail.com>
2025-10-29 18:26:59 +0000

    refactoring

--- tests/test_string_calculte.py
+++ tests/test_string_calculte.py
@@ -3,5 +3,5 @@ import pytest
 from str.string_calculte import stringcalcult
 
 @pytest.mark.parametrize("number,expected",[("0",0),("1",1),("2",2)])
-def test_calculte_string_0_return_0(number,expected):
+def test_calculte_string(number,expected):
     assert stringcalcult(number) == expected
