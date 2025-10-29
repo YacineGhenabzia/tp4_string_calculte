07845371666fa475ea0edace107284e62f93d59b
Yacine Ghenabzia <yacineghenabzia@gmail.com>
2025-10-29 18:20:06 +0000

    add test and code calculte string number 1

--- tests/test_string_calculte.py
+++ tests/test_string_calculte.py
@@ -3,3 +3,5 @@ from str.string_calculte import stringcalcult
 
 def test_calculte_string_0_return_0():
     assert stringcalcult("0")==0
+def test_calculte_string_1_return_1():
+    assert stringcalcult("1")==1
